516ac99baa882589637d2b045a7eda5ecb9e0ad7
Hirofumi Seiyama <hseiyama@gmail.com>
2026-04-01 05:05:01 +0900

    ra4m1_base: ベースプログラムを移植

--- e2_studio/ra4m1_base/ra_gen/pin_data.c
+++ e2_studio/ra4m1_base/ra_gen/pin_data.c
@@ -5,8 +5,11 @@
 const ioport_pin_cfg_t g_bsp_pin_cfg_data[] =
         {
 
-        { .pin = BSP_IO_PORT_01_PIN_05, .pin_cfg = ((uint32_t) IOPORT_CFG_IRQ_ENABLE
-                | (uint32_t) IOPORT_CFG_PORT_DIRECTION_INPUT) },
+        { .pin = BSP_IO_PORT_00_PIN_12, .pin_cfg = ((uint32_t) IOPORT_CFG_PORT_DIRECTION_OUTPUT
+                | (uint32_t) IOPORT_CFG_PORT_OUTPUT_LOW) },
+
+          { .pin = BSP_IO_PORT_01_PIN_05, .pin_cfg = ((uint32_t) IOPORT_CFG_IRQ_ENABLE
+                  | (uint32_t) IOPORT_CFG_PORT_DIRECTION_INPUT | (uint32_t) IOPORT_CFG_PULLUP_ENABLE) },
 
           { .pin = BSP_IO_PORT_01_PIN_08, .pin_cfg = ((uint32_t) IOPORT_CFG_PERIPHERAL_PIN
                   | (uint32_t) IOPORT_PERIPHERAL_DEBUG) },

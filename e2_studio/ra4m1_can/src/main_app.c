1fee57fbb9f39b31c2dee189160b4019f28fa5ca
Hirofumi Seiyama <hseiyama@gmail.com>
2026-04-09 04:13:17 +0900

    ra4m1_can: CAN受信処理を追加

--- e2_studio/ra4m1_can/src/main_app.c
+++ e2_studio/ra4m1_can/src/main_app.c
@@ -20,6 +20,7 @@
 #define UART_CMD_HELP		(0x08)					/* ヘルプ表示(^H)			*/
 #define UART_CMD_RESET		(0x12)					/* リセット(^R)				*/
 #define UART_CMD_SLEEP		(0x13)					/* スリープ(^S)				*/
+#define UART_CMD_CAN_TX		(0x14)					/* CAN送信(^T)				*/
 
 /* Private macro -------------------------------------------------------------*/
 
@@ -37,7 +38,9 @@ static can_frame_t g_can_rx_frame;
 
 /* Private function prototypes -----------------------------------------------*/
 static void can_init(void);							/* CAN 初期化処理						*/
-static void can_send(void);							/* CAN 送信処理							*/
+static void can_send(uint32_t id, uint8_t dlc, uint8_t data_id);
+													/* CAN 送信処理							*/
+static void uart_transfer(void);					/* UART 転送処理						*/
 
 /* Exported functions --------------------------------------------------------*/
 
@@ -115,6 +118,7 @@ void loop(void)
 			uartEchoStrln("^H :Help");
 			uartEchoStrln("^R :Reset");
 			uartEchoStrln("^S :Sleep");
+			uartEchoStrln("^T :CanTx");
 			break;
 		/* リセット(^R) */
 		case UART_CMD_RESET:
@@ -133,13 +137,25 @@ void loop(void)
 			/* 文字を出力する */
 			uartEchoStr("<Wakeup!!>");
 			break;
+		case UART_CMD_CAN_TX:
+			/* CAN 送信処理 */
+			can_send(0x1AB, 6, 0x40);
+			break;
 		}
+	}
+
+	/* CAN受信完了の通知があった場合 */
+	if (g_rx_flag) {
+		g_rx_flag = false;
+
+		/* UART 転送処理 */
+		uart_transfer();
 	}
 
 	/* 1秒判定時間が満了した場合 */
 	if (checkTimer(&sts_Timer1s, TIME_1S)) {
 		/* CAN 送信処理 */
-		can_send();
+		can_send(0x123, 8, 0x10);
 		/* ユーザーLEDを反転出力する */
 		R_PORT0->PODR_b.PODR12 = !R_PORT0->PODR_b.PODR12;
 		/* 文字を出力する */
@@ -181,26 +197,53 @@ static void can_init(void)
 
 /**
   * @brief  CAN 送信処理
+  * @param  id: CAN-ID
+  * @param  dlc: データ長
+  * @param  data_id: データに加算する識別子
+  * @retval None
+  */
+static void can_send(uint32_t id, uint8_t dlc, uint8_t data_id)
+{
+	g_can_tx_frame.id = id;
+	g_can_tx_frame.type = CAN_FRAME_TYPE_DATA;
+	g_can_tx_frame.data_length_code = dlc;
+	/* Write some data to the transmit frame */
+	g_can_tx_frame.data[0] = data_id + 0x01;
+	g_can_tx_frame.data[1] = data_id + 0x02;
+	g_can_tx_frame.data[2] = data_id + 0x03;
+	g_can_tx_frame.data[3] = data_id + 0x04;
+	g_can_tx_frame.data[4] = data_id + 0x05;
+	g_can_tx_frame.data[5] = data_id + 0x06;
+	g_can_tx_frame.data[6] = data_id + 0x07;
+	g_can_tx_frame.data[7] = data_id + 0x08;
+	/* Send data on the bus */
+	g_tx_flag  = false;
+	g_err_flag = false;
+	R_CAN_Write(&g_can0_ctrl, CAN_MAILBOX_ID_0, &g_can_tx_frame);
+}
+
+/**
+  * @brief  UART 転送処理
   * @param  None
   * @retval None
   */
-static void can_send(void)
+static void uart_transfer(void)
 {
-    g_can_tx_frame.id = 0x123;
-    g_can_tx_frame.type = CAN_FRAME_TYPE_DATA;
-    g_can_tx_frame.data_length_code = 8;
-    /* Write some data to the transmit frame */
-	g_can_tx_frame.data[0] = 0x11;
-	g_can_tx_frame.data[1] = 0x12;
-	g_can_tx_frame.data[2] = 0x13;
-	g_can_tx_frame.data[3] = 0x14;
-	g_can_tx_frame.data[4] = 0x15;
-	g_can_tx_frame.data[5] = 0x16;
-	g_can_tx_frame.data[6] = 0x17;
-	g_can_tx_frame.data[7] = 0x18;
-    /* Send data on the bus */
-    g_tx_flag  = false;
-    g_err_flag = false;
-    R_CAN_Write(&g_can0_ctrl, CAN_MAILBOX_ID_0, &g_can_tx_frame);
+	uint8_t u8_index;
+
+	/* Transfer can data to UART */
+	uartEchoStrln("");
+	uartEchoStr("CanRx id=");
+	uartEchoHex16((uint16_t)g_can_rx_frame.id);
+	uartEchoStr(" dlc=");
+	uartEchoHex8(g_can_rx_frame.data_length_code);
+	uartEchoStr(" data=");
+	for (u8_index = 0; u8_index < g_can_rx_frame.data_length_code; u8_index++) {
+		uartEchoHex8(g_can_rx_frame.data[u8_index]);
+		if (u8_index != (g_can_rx_frame.data_length_code - 1)) {
+			uartEchoStr(",");
+		}
+	}
+	uartEchoStrln("");
 }
 

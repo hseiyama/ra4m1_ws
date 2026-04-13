74b7cf02ee17f1779e1505575068dc276cafabc3
Hirofumi Seiyama <hseiyama@gmail.com>
2026-04-13 23:07:17 +0900

    ra4m1_can: 起動時のUART受信フレーミングエラーを回避

--- e2_studio/ra4m1_can/ra_gen/pin_data.c
+++ e2_studio/ra4m1_can/ra_gen/pin_data.c
@@ -21,7 +21,7 @@ const ioport_pin_cfg_t g_bsp_pin_cfg_data[] =
                   | (uint32_t) IOPORT_PERIPHERAL_SCI1_3_5_7_9) },
 
           { .pin = BSP_IO_PORT_01_PIN_10, .pin_cfg = ((uint32_t) IOPORT_CFG_PERIPHERAL_PIN
-                  | (uint32_t) IOPORT_PERIPHERAL_SCI1_3_5_7_9) },
+                  | (uint32_t) IOPORT_CFG_PULLUP_ENABLE | (uint32_t) IOPORT_PERIPHERAL_SCI1_3_5_7_9) },
 
           { .pin = BSP_IO_PORT_03_PIN_00, .pin_cfg = ((uint32_t) IOPORT_CFG_PERIPHERAL_PIN
                   | (uint32_t) IOPORT_PERIPHERAL_DEBUG) }, };
